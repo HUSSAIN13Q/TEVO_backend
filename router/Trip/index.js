64faac5d02e937343e3f0bbba74308ab9766a2f0
hussain <hussainalsaffar20@gmail.com>
2024-12-08 23:12:52 +0300

    create done

--- router/Trip/index.js
+++ router/Trip/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { getTripsRouter } = require("./getTrips");
+const { TripCreateRouter } = require("./createTrip");
 
 const router = express.Router();
 router.use(getTripsRouter);
+router.use(TripCreateRouter);
 
 module.exports = { TripsRouter: router };
