652020299e12ec86bab2072b3d192ee16174dbd5
hussain <hussainalsaffar20@gmail.com>
2024-12-08 09:24:07 +0300

    fix

--- models/User.js
+++ models/User.js
@@ -5,14 +5,6 @@ const PasswordManager = require("../helpers/PasswordManager");
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  image: { type: String },
-
-  myRecipes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Recipes",
-    },
-  ],
 });
 UserSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
