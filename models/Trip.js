5650156f1ed705f762ad0099f42cab916c16e907
abdulwahababdal <abdulwahababdal@gmail.com>
2024-12-10 10:56:54 +0300

    commit

--- models/Trip.js
+++ models/Trip.js
@@ -3,8 +3,8 @@ const { model, Schema } = require("mongoose");
 const TripSchema = new Schema({
   destination: { type: String, required: true },
   budget: { type: Number, required: true },
-  start_date: { type: Date },
-  end_date: { type: Date },
+  start_date: { type: String },
+  end_date: { type: String },
   //image: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User" },
 });
